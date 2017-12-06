c0aa349d600b340978fc647bdb396c09f06d58c3
stalwar7 <stalwar@cpp.edu>
2017-12-05 22:25:50 -0800

    progress on iterators

--- Iterators.java
+++ Iterators.java
@@ -1,5 +1,27 @@
 import java.util.Iterator;
 
-public class Iterators {
+public class Iterators<T> implements Iterator<T>{
+	private Iterator<T> head;
+	private Iterator<Iterator<T>> link; //initialize
+	
+	LinkedList<Integer> list = new LinkedList<Integer>();
+	LinkedStack<Integer> stack = new LinkedStack<Integer>();
+	LinkedQueue<Integer> queue = new LinkedQueue<Integer>();
+	
+	public Iterators(Iterable<Iterator<T>> iterators) {
+		this.link = iterators.iterator();
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
